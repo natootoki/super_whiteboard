cf66919f81539ccf41baa3230d96682754d02b00
natootoki <ikoan0226@gmail.com>
2023-08-28 00:21:23 +0900

    add panel

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let zure_y = -32;
 let filerecords = [];
 let filerecord = "";
 
+// 今のボードの状態を表す文字列
 let text_list = "";
 
 let para_ini = "para";
@@ -54,6 +55,15 @@ const selectFile = () => {
             paragraph.style.top = item.split(",")[1] + "px";
             // paragraph.style.left = 0 + "px";
             // paragraph.style.top = 0 + "px";
+
+            if (text_list != ""){
+                text_list += "\r\n"
+            }
+            text_list += item.split(",")[0]
+            text_list += ","
+            text_list += item.split(",")[1]
+            text_list += ","
+            text_list += item.split(",").slice(2).join(",")
         }
         console.log(filerecords)
 
@@ -67,6 +77,7 @@ const selectFile = () => {
     }
 }
 
+// ファイルエクスポート時の処理
 function OnButtonClick() {
     let blob = new Blob([text_list],{type:"text/plain"});
 
@@ -88,6 +99,123 @@ function OnButtonClick() {
     link.click();
 }
 
+// 取込用の処理
+function GetListClick() {
+    var textarea = document.getElementById("textarea");
+    textarea.value = text_list
+}
+
+// 反映用の処理
+function SetListClick() {
+    var textarea = document.getElementById("textarea");
+
+    const paragraphContainer = document.getElementById("paragraphContainer");
+    paragraphContainer.textContent = "";
+    myFunction()
+
+    text_list = "";
+    filerecords = textarea.value.split("\n")
+    //   filetext = test.split("\n")[2].split(",").slice(2).join(",");
+    for (const item of filerecords){
+        const paragraphContainer = document.getElementById("paragraphContainer");
+        const paragraph = document.createElement("span");
+        paragraph.textContent = item.split(",").slice(2).join(",").slice(1, -1);
+        para_id += 1;
+        paragraph.id = para_ini + para_id;
+        paragraphContainer.appendChild(paragraph);
+
+        // スタイルを設定して位置を指定
+        paragraph.style.position = "absolute";
+        paragraph.style.left = item.split(",")[0] + "px";
+        paragraph.style.top = item.split(",")[1] + "px";
+        // paragraph.style.left = 0 + "px";
+        // paragraph.style.top = 0 + "px";
+
+        if (text_list != ""){
+            text_list += "\r\n"
+        }
+        text_list += item.split(",")[0]
+        text_list += ","
+        text_list += item.split(",")[1]
+        text_list += ","
+        text_list += item.split(",").slice(2).join(",")
+    }
+}
+
+// 呼び出す関数の例
+function myFunction() {
+    const paragraphContainer = document.getElementById("paragraphContainer");
+    // contextmenu イベントが発生したときに実行される関数
+    paragraphContainer.addEventListener("contextmenu", function(event) {
+        event.preventDefault(); // コンテキストメニューを表示しない
+    });
+    paragraphContainer.textContent = "Hello, html!"
+
+    const paragraph = document.createElement("div");
+    paragraph.textContent = field.value;
+    paragraphContainer.appendChild(paragraph);
+    
+    // ファイルインポート用
+    const fileset = document.createElement("input");
+    fileset.id = "select-file"
+    fileset.type = "file";
+    fileset.setAttribute("onChange", "selectFile()");
+    paragraphContainer.appendChild(fileset);
+
+    // 改行
+    var br = document.createElement("br");
+    paragraphContainer.appendChild(br);
+
+    // ファイルエクスポート用
+    const fileget = document.createElement("input");
+    fileget.id = "get-file"
+    fileget.type = "button";
+    fileget.value = "エクスポート";
+    fileget.setAttribute("onClick", "OnButtonClick()");
+    paragraphContainer.appendChild(fileget);
+
+    // 改行
+    var br = document.createElement("br");
+    paragraphContainer.appendChild(br);
+
+    // 新しいテキストエリア要素を作成
+    var textarea = document.createElement("textarea");
+    textarea.rows = "4";
+    textarea.cols = "50";
+    textarea.id = "textarea";
+    textarea.placeholder = "ここにテキストを入力してください。";
+    paragraphContainer.appendChild(textarea);
+
+    // 改行
+    var br = document.createElement("br");
+    paragraphContainer.appendChild(br);
+
+    // 取り込み
+    const get_list = document.createElement("input");
+    get_list.id = "get-list"
+    get_list.type = "button";
+    get_list.value = "取込";
+    get_list.setAttribute("onClick", "GetListClick()");
+    paragraphContainer.appendChild(get_list);
+
+    // 反映
+    const set_list = document.createElement("input");
+    set_list.id = "set-list"
+    set_list.type = "button";
+    set_list.value = "反映";
+    set_list.setAttribute("onClick", "SetListClick()");
+    paragraphContainer.appendChild(set_list);
+
+    paragraphContainer.style.position = "absolute";
+    paragraphContainer.style.left = 0 + "px";
+    paragraphContainer.style.top = 0 + "px";
+    const windowWidth = window.innerWidth; // ウィンドウの幅
+    const windowHeight = window.innerHeight; // ウィンドウの高さ
+    paragraphContainer.style.width = windowWidth + "px"; // <div> の幅を設定
+    paragraphContainer.style.height = windowHeight + "px";
+}
+
+// ページロード時の処理
 document.addEventListener("DOMContentLoaded", function() {
     const inputContainer = document.getElementById("inputContainer");
     const inputField = document.createElement("input");
@@ -96,6 +224,7 @@ document.addEventListener("DOMContentLoaded", function() {
     inputField.placeholder = "テキストを入力してください";
     inputContainer.appendChild(inputField);
 
+    // 確定前にプレビューで表示させる
     const preview = document.createElement("span");
     preview.id = "preview";
     preview.textContent = field.value;
@@ -109,35 +238,7 @@ document.addEventListener("DOMContentLoaded", function() {
     inputContainer.style.left = xCoordinate + "px";
     inputContainer.style.top = yCoordinate + "px";
 
-    const paragraphContainer = document.getElementById("paragraphContainer");
-    const paragraph = document.createElement("div");
-    paragraph.textContent = field.value;
-    paragraphContainer.appendChild(paragraph);
-    
-    const fileset = document.createElement("input");
-    fileset.id = "select-file"
-    fileset.type = "file";
-    fileset.setAttribute("onChange", "selectFile()");
-    paragraphContainer.appendChild(fileset);
-
-    const br = document.createElement("br");
-    paragraphContainer.appendChild(br);
-
-    const fileget = document.createElement("input");
-    fileget.id = "get-file"
-    fileget.type = "button";
-    fileget.value = "エクスポート";
-    fileget.setAttribute("onClick", "OnButtonClick()");
-    paragraphContainer.appendChild(fileget);
-
-    paragraphContainer.style.position = "absolute";
-    paragraphContainer.style.left = 0 + "px";
-    paragraphContainer.style.top = 0 + "px";
-    const windowWidth = window.innerWidth; // ウィンドウの幅
-    const windowHeight = window.innerHeight; // ウィンドウの高さ
-    paragraphContainer.style.width = windowWidth + "px"; // <div> の幅を設定
-    paragraphContainer.style.height = windowHeight + "px";
-
+    myFunction()
 });
 
 function updateContainerSize() {
